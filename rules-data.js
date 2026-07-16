7353821594065923dec4c3ec60b1d5e4ce4cec2c
xr7zmk6669-creator <xr7zmk6669@privaterelay.appleid.com>
2026-07-16 12:16:14 -0700

    v1.6.6: remove CPT 45386 from known problem CPT list

--- rules-data.js
+++ rules-data.js
@@ -432,6 +432,5 @@
       { code: "J7301", description: "Levonorgestrel-releasing intrauterine contraceptive system (Skyla), 13.5 mg", dateAdded: "2026-06-17", ticket: "Pending" },
       { code: "Q0091", description: "Screening Papanicolaou smear; obtaining, preparing and conveyance of cervical or vaginal smear to laboratory", dateAdded: "2026-06-17", ticket: "Pending" },
       { code: "Q9967", description: "Low osmolar contrast material, 300-399 mg/ml iodine concentration, per ml", dateAdded: "2026-06-17", ticket: "Pending" },
-      { code: "45386", description: "Colonoscopy, flexible; with transendoscopic balloon dilation", dateAdded: "2026-06-17", ticket: "Pending" },
       { code: "44394", description: "Colonoscopy through stoma; with removal of tumor(s), polyp(s), or other lesion(s) by snare technique", dateAdded: "2026-06-17", ticket: "Pending" }
     ];
